66b7329da8e89e647b23b00a561ac8e015c34765
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-07-18 19:03:12 +0200

    handle invalid id

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,6 @@
 import User from "../models/user";
 import { Request, Response, NextFunction } from "express";
+import { userNotFounded } from "../errorCodes";
 
 export function index(req: Request, res: Response, next: NextFunction): void {
   User.find()
@@ -17,20 +18,35 @@ export function create(req: Request, res: Response, next: NextFunction): void {
 export function show(req: Request, res: Response, next: NextFunction): void {
   User.findById(req.params.id)
     .exec()
-    .then((user) => res.json(user))
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      }
+      res.status(404).json(userNotFounded);
+    })
     .catch((err) => next(err));
 }
 
 export function update(req: Request, res: Response, next: NextFunction): void {
   User.findByIdAndUpdate(req.params.id, req.body)
     .exec()
-    .then((user) => res.json(user))
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      }
+      res.status(404).json(userNotFounded);
+    })
     .catch((err) => next(err));
 }
 
 export function destroy(req: Request, res: Response, next: NextFunction): void {
   User.findByIdAndDelete(req.params.id)
     .exec()
-    .then((user) => res.json(user))
+    .then((user) => {
+      if (user) {
+        res.json(user);
+      }
+      res.status(404).json(userNotFounded);
+    })
     .catch((err) => next(err));
 }
