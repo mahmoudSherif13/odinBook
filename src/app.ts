938a7a43ff3dfbaf7015479b5452ee3fc5149a3a
mahmoodSherif <mahmoodSherif13@gmail.com>
2020-07-26 02:10:59 +0200

    add Access-Control-Allow-Origin

--- src/app.ts
+++ src/app.ts
@@ -22,6 +22,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/", router);
 
 export default app;
